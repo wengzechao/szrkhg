95fac3b0b9e7f0867d5ab10258aac47577605c67
wengzechao <1020028174@qq.com>
2017-07-02 20:11:54 +0800

    add

---
+++ js/faq.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    if (window.screen.width<=425)
+     {
+        document.getElementById("zc").innerHTML="Frequently Asked Questions";
+     }
+});
---
+++ js/styles.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	if (window.screen.width>=768)
+	 {
+	 	document.getElementById("zc").innerHTML="CHOOSE YOUR MASSAGE TYPE";
+	 }
+});
